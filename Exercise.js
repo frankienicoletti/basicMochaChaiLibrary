f2d5131933ec06d1e503f65426c5aabecdbec23d
Kate Jefferson <kjefferson@kjefferson-mac.local>
2015-08-26 19:04:42 -0700

    initial commit

---
+++ Exercise.js
@@ -0,0 +1,38 @@
+/**
+ *
+ */
+
+var FIX_ME = 0;
+var answers = [];
+
+// Replace the FIX_ME variable with the correct answer to make the expression true
+answers[0] = "100" === FIX_ME;
+answers[1] = 85 % FIX_ME === 4;
+answers[2] = !(1 < 2 && 4 !== 5) || undefined === FIX_ME;
+answers[3] = FIX_ME ? true : false;
+
+var mini_array = [1, 2, 3];
+answers[4] = mini_array[2] === FIX_ME;
+answers[5] = mini_array.length === FIX_ME;
+
+var mini_object = {name: apple, color: red, edible: true};
+answers[6] = mini_object[FIX_ME] === 'red';
+answers[7] = mini_object.length === FIX_ME;
+
+// Write a function that accepts two numbers are returns their sum
+// Modify the function to accept a third number
+function addition(num1, num2) {
+
+}
+
+// Write a function that accepts two numbers and adds them together if they are even
+// or multiplies them if they are odd
+function fun_with_math(num1, num2) {
+
+}
+
+// Write the ForEach function that accepts a collection (array or object) and performs
+// callback function on each item in it
+function ForEach(collection, callback) {
+
+}
--- Spec.js
+++ Spec.js
@@ -1,48 +1,73 @@
-describe("treeMap", function() {
+describe("Data Types, Operators, & Expressions", function() {
 
-  it("should not modify the original tree", function(){
-    var root1 = new Tree(1);
-    var branch2 = root1.addChild(2);
-    var branch3 = root1.addChild(3);
-    var leaf4 = branch2.addChild(4);
-    var root2 = root1.map(function(value) {return value*2;});
-    expect(root1.value).to.eql(1);
-    expect(root1.children[0].children[0].value).to.eql(4);
+  it("should return true", function(){
+    expect(answers[0]).to.eql(true);
   });
 
-  it("should apply callback to all tree nodes", function(){
-    var root1 = new Tree(1);
-    var branch2 = root1.addChild(2);
-    var branch3 = root1.addChild(3);
-    var leaf4 = branch2.addChild(4);
-    var root2 = root1.map(function(value) {return value*2;});
-    expect(root2.value).to.eql(2);
-    expect(root2.children[0].children[0].value).to.eql(8);
+  it("should return true", function(){
+    expect(answers[1]).to.eql(true);
   });
 
-});
-
-describe("mapInPlace", function() {
-
-  it("should modify the original tree", function(){
-    var root1 = new Tree(1);
-    var branch2 = root1.addChild(2);
-    var branch3 = root1.addChild(3);
-    var leaf4 = branch2.addChild(4);
-    root1.mapInPlace(function(value) {return value*2;});
-    expect(root1.value).to.eql(2);
-    expect(root1.children[0].children[0].value).to.eql(8);
+  it("should return true", function(){
+    expect(answers[2]).to.eql(true);
   });
 
-  it("should apply callback to all tree nodes", function(){
-    var root1 = new Tree(1);
-    var branch2 = root1.addChild(2);
-    var branch3 = root1.addChild(3);
-    var leaf4 = branch2.addChild(4);
-    root1.mapInPlace(function(value) {return value*2;});
-    expect(root1.value).to.eql(2);
-    expect(root1.children[0].children[0].value).to.eql(8);
+  it("should return true", function(){
+    expect(answers[3]).to.eql(true);
+  });
+
+  it("should return true", function(){
+    expect(answers[4]).to.eql(true);
+  });
+
+  it("should return true", function(){
+    expect(answers[5]).to.eql(true);
+  });
+
+  it("should return true", function(){
+    expect(answers[6]).to.eql(true);
+  });
+
+  it("should return true", function(){
+    expect(answers[7]).to.eql(true);
   });
 
 });
 
+describe("Basic Functions", function() {
+
+  it("should add 2 things", function() {
+    expect(addition(2, 2)).to.eql(4);
+  });
+
+  it("should add 3 things", function() {
+    expect(addition(2, 3, 4)).to.eql(9);
+  });
+
+  it("should include a conditional statement", function() {
+    expect(fun_with_math(2, 2)).to.eql(4);
+  });
+
+  it("should do something different with odd numbers", function() {
+    expect(fun_with_math(3, 3)).to.eql(9);
+  })
+
+});
+
+describe("Higher Order Function", function() {
+
+  it("should multiple numbers in an array", function() {
+    var test_array = [1, 2, 3, 4, 5];
+    expect(ForEach(test_array, function(item) {
+      item = item * 2;
+    })).to.eql([2, 4, 6, 8, 10]);
+  });
+
+  it("should add y to all the strings", function() {
+    var test_array = ['cat', 'bat', 'hat'];
+    expect(ForEach(test_array, function(item) {
+      item = item + 'y';
+    })).to.eql(['caty', 'baty', 'haty']);
+  });
+
+});
